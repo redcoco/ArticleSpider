fbbe34a963d924fcb2a8db496952d82410950886
redcoco <1849737243@qq.com>
2017-08-25 22:33:59 +0800

    pycharm 调试scrapy 执行流程

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -4,8 +4,8 @@ import scrapy
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
-    allowed_domains = ['jobbole.com']
-    start_urls = ['http://jobbole.com/']
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ['http://blog.jobbole.com/']
 
     def parse(self, response):
         pass
